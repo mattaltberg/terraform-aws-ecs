a8fc2a240aad8fdfbd9c02f921de03228d83b9d0
Matthew Altberg <matthew.altberg@strong.io>
2024-05-14 09:23:14 -0400

    feat: add support for custom metric queries in customized metric spec

--- modules/service/main.tf
+++ modules/service/main.tf
@@ -776,7 +776,6 @@ resource "aws_iam_role" "task_exec" {
   description = coalesce(var.task_exec_iam_role_description, "Task execution role for ${local.task_exec_iam_role_name}")
 
   assume_role_policy    = data.aws_iam_policy_document.task_exec_assume[0].json
-  max_session_duration  = var.task_exec_iam_role_max_session_duration
   permissions_boundary  = var.task_exec_iam_role_permissions_boundary
   force_detach_policies = true
 
@@ -1247,6 +1246,37 @@ resource "aws_appautoscaling_policy" "this" {
         for_each = try([target_tracking_scaling_policy_configuration.value.customized_metric_specification], [])
 
         content {
+          dynamic "metrics"{
+            for_each = try(customized_metric_specification.value.metrics, [])
+            content {
+              id = metrics.value.id
+              label = try(metrics.value.label, null)
+              return_data = try(metrics.value.return_data, true)
+              expression = try(metrics.value.expression, null)
+
+
+              dynamic "metric_stat" {
+                for_each = try([metrics.value.metric_stat], [])
+                content {
+                  stat = metric_stat.value.stat
+                  dynamic "metric" {
+                    for_each = try([metric_stat.value.metric], [])
+                    content {
+                      namespace = metric.value.namespace
+                      metric_name = metric.value.metric_name
+                      dynamic "dimensions" {
+                        for_each = try(metric.value.dimensions, [])
+                        content {
+                          name = dimensions.value.name
+                          value = dimensions.value.value
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
           dynamic "dimensions" {
             for_each = try(customized_metric_specification.value.dimensions, [])
 
@@ -1256,9 +1286,9 @@ resource "aws_appautoscaling_policy" "this" {
             }
           }
 
-          metric_name = customized_metric_specification.value.metric_name
-          namespace   = customized_metric_specification.value.namespace
-          statistic   = customized_metric_specification.value.statistic
+          metric_name = try(customized_metric_specification.value.metric_name, null)
+          namespace   = try(customized_metric_specification.value.namespace, null)
+          statistic   = try(customized_metric_specification.value.statistic, null)
           unit        = try(customized_metric_specification.value.unit, null)
         }
       }
